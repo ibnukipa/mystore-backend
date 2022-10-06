fe4526f82f77447123c6fd4d88b92c19906ba28f
Ibnu Prayogi <ibnu.dev@gmail.com>
2022-10-06 20:42:41 +0700

    feat: exporting the db based on knex config

--- src/database/index.js
+++ src/database/index.js
@@ -1,2 +1,7 @@
 const knex = require('knex')
 const knexFile = require('knexfile')
+
+// TODO: the knexfile.development should be configurable based on env (dev, staging, or prod)
+const db = knex(knexFile.development)
+
+module.exports = db
